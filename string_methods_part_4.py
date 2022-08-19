74d2b624af189b626f2361bfcaebf368797ddf37
RajputNandini <109655286+nandinirajput@users.noreply.github.com>
2022-08-19 14:33:58 +0530

    Add files via upload

---
+++ string_methods_part_4.py
@@ -0,0 +1,23 @@
+# print(bin(5)) #0b101 (or) 0B101.binary with is divisible with only the 2 table only.
+# print(bin(10)) #0B1010
+# print(bin(8))  #0b1000
+# print(bin(7))  #0b111
+# print(bin(1))  #0b1
+# print(bin(0))  #0b0
+# print(0b101)  #5.to check weather the answer 5 is correct or not.
+
+
+# print(oct(36))  #0o44 (or) 0o44.octal which is divisible with only the 8 table only.
+# print(oct(40))  #0o50 (Or) 0o50
+# print(0o50)  #40.to check weather the 40 answer is correct or not 
+
+
+# print(hex(50))  #0x32 (or) 0x32,hexa decimal is divisible with only the 16 table only.
+# print(0x32)  #50.to check weather the answer 50 is correct or not and its correct.
+# print(0xA)  #10
+# print(0xb)  #11
+# print(0xc)  #12
+# print(0xd)  #13
+# print(0xE)  #14
+# print(0xF)  #15. in hexa decimal till A to F it may be upper case letter or lower case letter it contain some variable 
+#this hexa decimal values containtill A to F.and only in hex method only.
